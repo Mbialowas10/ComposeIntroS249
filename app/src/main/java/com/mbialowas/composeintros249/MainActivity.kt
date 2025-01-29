6738fd1b9259aaf243a874913a7259a011588f5e
Michael Bialowas <mbialowas@rrc.ca>
2025-01-29 09:14:10 -0600

    Preview Of Some Composable Functions_s249

--- app/src/main/java/com/mbialowas/composeintros249/MainActivity.kt
+++ app/src/main/java/com/mbialowas/composeintros249/MainActivity.kt
@@ -1,14 +1,24 @@
 package com.mbialowas.composeintros249
 
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material3.Button
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Switch
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.mbialowas.composeintros249.ui.theme.ComposeIntroS249Theme
@@ -20,28 +30,73 @@ class MainActivity : ComponentActivity() {
         setContent {
             ComposeIntroS249Theme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+                    // entry point into our application
+                    Column {
+                        Counter(modifier = Modifier.padding(innerPadding))
+                        Switcher()
+                        CustomList()
+                    }
+
                 }
             }
         }
+
+    }
+}
+@Composable
+fun Counter(modifier: Modifier = Modifier){
+    var counter by remember{
+        mutableStateOf(10)
+    }
+
+    Button(
+         onClick = {
+             counter += 1000
+             Log.i("MJB", "Counter: $counter")
+         }
+    ){
+        Text(text = "I've been clicked $counter times")
     }
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+fun Switcher(){
+    val checked = remember{
+        mutableStateOf(false)
+    }
+    Column() {
+        Switch(
+            checked = checked.value,
+            onCheckedChange = {
+                checked.value = it
+            }
+        )
+        if (checked.value){
+            Text(text = "Switch is on")
+        }else{
+            Text(text = "Switch is off")
+        }
+    }
+
+}
+@Composable
+fun CustomList(){
+    val itemList = List(200){"Control $it"}
+    LazyColumn{
+        items(itemList){
+            Text(text = it)
+        }
+    }
+
 }
 
-@Preview(showBackground = true)
 @Composable
-fun GreetingPreview() {
-    ComposeIntroS249Theme {
-        Greeting("Android")
+fun  MyCheckbox(){
+    var checked by remember {
+        mutableStateOf(false)
     }
+
 }
+
+
+
